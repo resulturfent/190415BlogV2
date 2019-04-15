609674ab65d0b69522d1017c7b1893e558255948
aslı duman <asli_duman_466@hotmail.com>
2019-04-15 10:58:33 +0300

    aslıduman controller ekle

---
+++ 190410_BlogV1/Controllers/aslıController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _190410_BlogV1.Controllers
+{
+    public class aslıController : Controller
+    {
+        // GET: aslı
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
